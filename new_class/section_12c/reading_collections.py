6b8d93512816f617cbc0857f88e11f6c801b8058
girlycoded <imjoosan@gmail.com>
2026-08-03 11:00:44 -0600

    Section 12 Classwork

--- new_class/section_12c/reading_collections.py
+++ new_class/section_12c/reading_collections.py
@@ -1,3 +1,49 @@
 #name: joosan
 #desc: reading collections
 
+import ast
+from json import dumps # USED ONLY FOR PRINTING PRETTY
+
+indent_char = "  "
+
+loaded_things = []
+with open("things.txt", "r") as file_in:
+    for line in file_in:
+        clean_line = line.strip("\n")
+        loaded_things.append(clean_line)
+print(dumps(loaded_things, indent=indent_char))
+
+more_loaded_things = []
+with open("things_in_line.txt", "r") as file_in:
+    for item in file_in.read().replace("\n", ", ").split(", "):
+        more_loaded_things.append(item)
+print(dumps(more_loaded_things, indent=indent_char))
+
+print("Done reading lists!!!!!!!!!!!!!!!!!!!!!\n")
+
+more_loaded_things = {}
+with open("dict.txt", "r") as file_in:
+    for item in file_in:
+        key, value = item.rstrip("\n").split(":")
+        more_loaded_things[key] = value
+print(dumps(more_loaded_things, indent=indent_char))
+
+city_locations = {}
+with open("city_locations.txt", "r") as file_in:
+    for item in file_in:
+        city, lon, lat = item.rstrip("\n").split(":")
+        more_loaded_things[city] = (lon, lat)
+print(dumps(more_loaded_things, indent=indent_char))
+
+with open("tuple_things.txt", "r") as file_in:
+    tuple_things = tuple(file_in.read().strip("(").strip(")").rstrip("\n").split(", "))
+    print(tuple_things)
+
+with open("mixed_tuple.txt", "r") as file_in:
+    loaded_tuple = ast.literal_eval(file_in.read())
+    print(loaded_tuple)
+
+# yes i was lazy with this :P
+with open("things_set.txt", "r") as file_in:
+    loaded_map = ast.literal_eval(file_in.read())
+    print(loaded_map)
--- new_class/section_12c/writing_collections.py
+++ new_class/section_12c/writing_collections.py
@@ -1,6 +1,8 @@
 #name: joosan
 #desc: write collections to file
 
+
+# lists
 things = ["laptop", "coffee", "book", "keys"]
 
 with open("things.txt", "w") as file_out:
@@ -10,4 +12,43 @@ with open("things.txt", "w") as file_out:
 with open("things_in_line.txt", "w") as file_out:
     file_out.write(", ".join(things))
 
-print("Writing list completed!")
+# dicts
+dict_of_things = {
+    "laptop": "device for gaming", 
+    "coffee_mug": "vessel holding morning fuel",
+    "backpack": "bag containing a laptop and coffee",
+    "desk_lamp": "thing to kill pixar I",
+    "keys": "metal tools used to unlock your front door",
+}
+
+with open("dict.txt", "w") as file_out:
+    # file_out.write("{\n")
+    for i, v in dict_of_things.items():
+        file_out.write(f'{i}:{v}\n')
+    # file_out.write("}")
+
+# tuple
+city_locations = {
+    "New York": (40.7128, -74.0060),
+    "Tokyo": (35.6764, 139.6500),
+    "Paris": (48.8566, 2.3522),
+}
+
+with open("city_locations.txt", "w") as file_out:
+    for key, value in city_locations.items():
+        file_out.write(f"{key}:{value[0]}:{value[1]}\n")
+
+
+things_tuple = (10, 20, 30)
+with open("tuple_things.txt", "w") as file_out:
+    file_out.write(f"{things_tuple}")
+
+things_mixed_tuple = ("apple", 42, 3.14, True)
+with open("mixed_tuple.txt", "w") as file_out:
+    file_out.write(str(things_mixed_tuple))
+
+things_set = {"red", "orange", "yellow", "green", "blue", "purple", "pink"}
+with open("things_set.txt","w") as file_out:
+    file_out.write(f"{things_set}")
+
+print("Writing completed!")
